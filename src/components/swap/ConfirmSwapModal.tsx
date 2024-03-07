ee2ed2303a6aea8ad812fbd0f08cda1277acf0cb
zhichunchun <zhichunchun@iqingmiao.com>
2024-03-07 14:54:27 +0800

    Modify

--- src/components/swap/ConfirmSwapModal.tsx
+++ src/components/swap/ConfirmSwapModal.tsx
@@ -1,4 +1,4 @@
-import { currencyEquals, Trade } from '@uniswap/sdk'
+import { currencyEquals, Trade } from '@b-chunchun/uniswap-sdk'
 import React, { useCallback, useMemo } from 'react'
 import TransactionConfirmationModal, {
   ConfirmationModalContent,
--- src/state/transactions/actions.ts
+++ src/state/transactions/actions.ts
@@ -1,5 +1,5 @@
 import { createAction } from '@reduxjs/toolkit'
-import { ChainId } from '@uniswap/sdk'
+import { ChainId } from '@b-chunchun/uniswap-sdk'
 
 export interface SerializableTransactionReceipt {
   to: string
